f2049fd2908cbe6482aaf0b0f155b90326ece6e5
zhn662 <zhn662@188.com>
2014-07-13 17:44:38 +0800

    add localStorage code snippets.

---
+++ snippets/localstorage.js
@@ -0,0 +1,26 @@
+var localStorage = {
+    isSupports : function() {
+        return ('localStorage' in window) && window.localStorage !== null;
+    },
+    setItem : function(name, value) {
+        if (this.isSupports()) {
+            window.localStorage.setItem(name, value);
+        }
+    },
+    getItem : function(name) {
+        if (this.isSupports()) {
+            return window.localStorage.getItem(name);
+        }
+        return null;
+    },
+    removeItem : function(name) {
+        if (this.isSupports()) {
+            window.localStorage.removeItem(name);
+        }
+    },
+    clear : function() {
+        if (this.isSupports()) {
+            window.localStorage.clear();
+        }
+    }
+};
