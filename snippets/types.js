a9c81a3c40d52353723eb9e41b315fec10e89f97
zhn662 <zhn662@188.com>
2014-08-08 00:06:58 +0800

    make some changes to the comment formats.

--- snippets/types.js
+++ snippets/types.js
@@ -1,12 +1,14 @@
-var types = {
+var typeUtils = {
     getType : function(obj) {
-        //undefined/null结果为[object Undefined/Null], 在IE8下返回结果均为[object Object].
+        // undefined/null结果为[object Undefined/Null], 在IE8下返回结果均为[object Object].
         var type = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
         if (type === 'object') {
             if ( typeof obj === 'undefined') {
                 type = 'undefined';
             } else if (obj === null) {
                 type = 'null';
+            } else if (this.isArguments(obj)) {
+                type = 'arguments';
             }
         }
         return type;
@@ -15,27 +17,34 @@ var types = {
         return this.getType(obj) === type;
     },
     isArray : function(obj) {
-        return Array.isArray(obj) || this.isType('array', obj);
-        //return obj instanceof Array;
-        //return obj.constructor === 'Array';
-        //return this.getType(obj) === 'array';
+        return Array.isArray && Array.isArray(obj) || this.isType('array', obj);
+        // return obj instanceof Array;
+        // return obj.constructor === 'Array';
+        // return this.getType(obj) === 'array';
     },
     isArrayLike : function(obj) {
         return obj && typeof obj.length === 'number';
-        //return obj !== undefined && obj !== null && typeof obj.length === 'number';
+        // return obj !== undefined && obj !== null && typeof obj.length === 'number';
     },
     isArguments : function(obj) {
-        //判断变量的类型是否是 Arguments
+        // 判断变量的类型是否是 Arguments
         return obj && obj.callee && this.isArrayLike(obj) ? true : false;
     },
     isFunction : function(obj) {
         return this.isType('function', obj);
-        //return obj.constructor === 'Function';
-        //return this.getType(obj) === 'function';
+        // return obj.constructor === 'Function';
+        // return this.getType(obj) === 'function';
     },
     isObject : function(obj) {
         return this.isType('object', obj);
-        //return this.getType(obj) === 'object';
+        // return this.getType(obj) === 'object';
+    },
+    isEmptyObject : function(obj) {
+        for (var key in obj) {
+            return false;
+        }
+        return true;
+        // return Object.keys(obj).length > 0 ? false : true;
     },
     isPlainObject : function(obj) {
         return this.isObject(obj) && !this.isWindow(obj) && Object.getPrototypeOf(obj) === Object.prototype;
